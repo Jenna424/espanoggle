e89a7290c6a956f721342b69417c31e3112974a6
github username <github email address>
2019-03-16 17:13:58 -0400

    stub out BoggleGame container class component

---
+++ src/containers/BoggleGame.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class BoggleGame extends Component {
+  render() {
+  	return (
+  	  <div>Stubbing out the shell of my BoggleGame React container class component!</div>
+  	)
+  }
+}
+
+export default BoggleGame;
