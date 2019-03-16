ff55d0b1fcc970ca708e75a54dfc60577e5e220f
github username <github email address>
2019-03-16 17:36:26 -0400

    define shakeDiceTray to shuffle array of string dice

--- src/containers/BoggleGame.js
+++ src/containers/BoggleGame.js
@@ -19,6 +19,17 @@ const sixteenDice = [
   'UOTOÑN'
 ];
 
+// I'm using Fisher Yates algorithm to destructively shuffle the sixteenDice array of 16 strings
+const shakeDiceTray = diceArray => {
+  for (let i = 0; i < 16; i++) {
+    let arbitraryIndex = Math.floor(Math.random() * 16);
+    let temporaryValue = diceArray[i];
+    diceArray[i] = diceArray[arbitraryIndex];
+    diceArray[arbitraryIndex] = temporaryValue;
+  }
+  return diceArray;
+}
+
 class BoggleGame extends Component {
   render() {
   	return (
