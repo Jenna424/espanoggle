7a361b87ee814d2973a0dd490f1af209f7e73845
github username <github email address>
2019-03-16 20:41:37 -0400

    set up index.js file

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import BoggleGame from './containers/BoggleGame';
+import './index.css';
+
+ReactDOM.render(
+  <BoggleGame />,
+  document.getElementById('root')
+);
