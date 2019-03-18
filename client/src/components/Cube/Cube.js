2b5b6b9992d82a83e54f9b9acfdc4d20dd736924
github username <github email address>
2019-03-18 17:49:41 -0400

    fix formatting of Cube.js

--- client/src/components/Cube/Cube.js
+++ client/src/components/Cube/Cube.js
@@ -1,17 +1,24 @@
-import React, { PureComponent } from 'react'; // PureComponent executes shouldComponentUpdate lifecycle method behind the scenes
+{/*
+// PureComponent executes shouldComponentUpdate lifecycle method behind the scenes
+// I'm using PureComponent here to avoid unnecessary re-renders of every cube in the boggle board when one is clicked. 
+// Only the cube that was clicked (and maybe some adjacent cubes) should re-render.
+*/}
+
+import React, { PureComponent } from 'react';
 import styles from './Cube.module.css';
-// I'm using PureComponent here to avoid unnecessary re-renders of every cube in the boggle board when one is clicked. Only the cube that was clicked (and maybe some adjacent cubes) should re-render.
+
 class Cube extends PureComponent {
   handleOnClick = () => {
-    //if (this.props.isClickable) {
+    if (this.props.isClickable(this.props.cube)) {
       this.props.handleCubeClicked(this.props.cube)
-    //}
+    }
   }
 
   render() {
     return (
-      <button className={`big ui button ${styles.cube}`} onClick={this.handleOnClick} disabled={!this.props.isClickable}>
-        {this.props.character}
+      <button 
+        className={`big ui button ${styles.cube}`} 
+        onClick={this.handleOnClick}>{this.props.character}
       </button>
     )
   }
