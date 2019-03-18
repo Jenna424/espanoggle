10c6b05361f22926fcb08178e5b705d9d95fbc11
github username <github email address>
2019-03-18 13:28:13 -0400

    fix handleOnClick in Cube component

--- client/src/components/Cube/Cube.js
+++ client/src/components/Cube/Cube.js
@@ -3,17 +3,17 @@ import styles from './Cube.module.css';
 // I'm using PureComponent here to avoid unnecessary re-renders of every cube in the boggle board when one is clicked. Only the cube that was clicked (and maybe some adjacent cubes) should re-render.
 class Cube extends PureComponent {
   handleOnClick = () => {
-  	if (this.props.isClickable) {
-  	  this.props.handleCubeClick()
-  	}
+    if (this.props.isClickable) {
+      this.props.handleCubeClicked(this.props.cube)
+    }
   }
 
   render() {
-  	return (
-  	  <button className={`big ui button ${styles.cube}`} onClick={this.handleOnClick}>
-  	    {this.props.character}
-  	  </button>
-  	)
+    return (
+      <button className={`big ui button ${styles.cube}`} onClick={this.handleOnClick}>
+        {this.props.character}
+      </button>
+    )
   }
 }
 
