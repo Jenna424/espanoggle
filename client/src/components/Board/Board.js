e14d004f9f0bd036cc6b1d2f0c168443c9902be2
github username <github email address>
2019-03-17 20:32:50 -0400

    define Board functional component in Board.js

--- client/src/components/Board/Board.js
+++ client/src/components/Board/Board.js
@@ -2,6 +2,25 @@ import React from 'react';
 import Cube from '../Cube/Cube';
 import styles from './Board.module.css';
 
+const Board = ({ board }) => {
+  return (
+    <div className={styles.board}>
+      {board.map((row, rowIndex) => 
+        <div key={rowIndex} style={{display: 'flex'}}>
+          {row.map(cubeObject => {
+            return (
+              <Cube 
+                key={cubeObject.c} 
+                rowNum={cubeObject.r} 
+                colNum={cubeObject.c} 
+                character={cubeObject.landedLetter} />
+            )
+          })}
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Board;
 
@@ -10,14 +29,14 @@ export default Board;
 // When Board component is rendered from BoggleGame's render method, it receives this object as its props:
 // {
 //   board: the array of nested arrays stored in BoggleGame's local state,
-//   handleOnClick: callback arrow function object (I'll have to pass this down further to Cube component)
+//   handleOnClick: callback arrow function object (I must pass this down again to Cube component)
 // }
 // .map() invokes the provided callback function once on each array element & returns an array of resulting values
 // Using .map(), iterate through the 4 row array elements of the outermost array
-// Call .map() on each row. For each cube object element in each row, which looks something like this 
-// { r: row number here, c: column number here, landedLetter: "string letter that landed face up here" },
+// Call .map() on each row. For each of 4 cube object elements in each row, which looks something like this 
+// { r: row number, c: column number, landedLetter: "string letter that landed face up" },
 // render a Cube functional component
-// When each Cube component is rendered from inside the .map() iteration, it receives this object as its props:
-// { rowNum: row number here, colNum: column number here, character: "string letter that landed face up here" }
+// When each Cube child component is rendered from inside the .map() iteration, it receives this object as its props:
+// { rowNum: row number, colNum: column number, character: "string letter that landed face up" }
 // The resulting JSX from rendering the array of Cube components will be injected into the <div> to populate the game board
 */
