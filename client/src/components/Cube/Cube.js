b8305c9e90989fd8a62ed21c54c8a25310d64061
github username <github email address>
2019-03-18 15:43:47 -0400

    add disabled property to <button> in Cube.js

--- client/src/components/Cube/Cube.js
+++ client/src/components/Cube/Cube.js
@@ -3,14 +3,14 @@ import styles from './Cube.module.css';
 // I'm using PureComponent here to avoid unnecessary re-renders of every cube in the boggle board when one is clicked. Only the cube that was clicked (and maybe some adjacent cubes) should re-render.
 class Cube extends PureComponent {
   handleOnClick = () => {
-    if (this.props.isClickable) {
+    //if (this.props.isClickable) {
       this.props.handleCubeClicked(this.props.cube)
-    }
+    //}
   }
 
   render() {
     return (
-      <button className={`big ui button ${styles.cube}`} onClick={this.handleOnClick}>
+      <button className={`big ui button ${styles.cube}`} onClick={this.handleOnClick} disabled={!this.props.isClickable}>
         {this.props.character}
       </button>
     )
