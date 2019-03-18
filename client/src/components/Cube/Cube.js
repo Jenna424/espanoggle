f49b488ae6aa86e26ccc8b96a87265263d352cb7
github username <github email address>
2019-03-18 11:41:59 -0400

    write the code I wish I had for Cube.js file

---
+++ client/src/components/Cube/Cube.js
@@ -0,0 +1,20 @@
+import React, { PureComponent } from 'react'; // PureComponent executes shouldComponentUpdate lifecycle method behind the scenes
+import styles from './Cube.module.css';
+// I'm using PureComponent here to avoid unnecessary re-renders of every cube in the boggle board when one is clicked. Only the cube that was clicked (and maybe some adjacent cubes) should re-render.
+class Cube extends PureComponent {
+  handleOnClick = () => {
+  	if (this.props.isClickable) {
+  	  this.props.handleCubeClick()
+  	}
+  }
+
+  render() {
+  	return (
+  	  <button className={`big ui button ${styles.cube}`} onClick={this.handleOnClick}>
+  	    {this.props.character}
+  	  </button>
+  	)
+  }
+}
+
+export default Cube;
