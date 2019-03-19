5951f89a17570d3cba5305760ea82aefbd4905fb
github username <github email address>
2019-03-19 13:15:46 -0400

    change button size in Cube.js

--- client/src/components/Cube/Cube.js
+++ client/src/components/Cube/Cube.js
@@ -1,12 +1,6 @@
 import React, { PureComponent } from 'react';
 import styles from './Cube.module.css';
 
-{/*
-// PureComponent executes shouldComponentUpdate lifecycle method behind the scenes
-// I'm using PureComponent here to avoid unnecessary re-renders of every cube in the boggle board when one is clicked. 
-// Only the cube that was clicked (and maybe some adjacent cubes) should re-render.
-*/}
-
 class Cube extends PureComponent {
   handleOnClick = () => {
     if (this.props.isClickable(this.props.cube)) {
@@ -16,12 +10,12 @@ class Cube extends PureComponent {
 
   render() {
     return (
-      <button 
-        className={`big ui button ${styles.cube}`} 
-        onClick={this.handleOnClick}>{this.props.character}
-      </button>
+      <div>
+        <button 
+          className={`tiny ui button ${styles.cube}`} 
+          onClick={this.handleOnClick}>{this.props.character}
+        </button>
+      </div>
     )
   }
 }
-
-export default Cube;
