7ffac67969a5adb2ac66ff9b962d22c3cbec5261
github username <github email address>
2019-03-20 14:45:40 -0400

    import Button, add enviarPalabra prop, render ENVIAR button, add space

--- client/src/components/PalabraPresentada/PalabraPresentada.js
+++ client/src/components/PalabraPresentada/PalabraPresentada.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Button from '../UI/Button/Button';
 
-const PalabraPresentada = ({ palabraCreada }) => {
+const PalabraPresentada = ({ palabraCreada, enviarPalabra }) => {
   return (
   	<div style={{textAlign: 'center', paddingTop: '10px'}}>
-  	  {palabraCreada.length > 0 && <p style={{color: 'navy'}}><strong>Palabra Creada</strong>: {palabraCreada}</p>}
+  	  {palabraCreada.length > 0 && <p style={{color: 'navy'}}><strong>Palabra Creada</strong>:&nbsp;{palabraCreada}</p>}
+  	  {palabraCreada.length >= 3 && <Button onClick={enviarPalabra} buttonType="enviar">ENVIAR</Button>}
   	</div>
   )
 }
