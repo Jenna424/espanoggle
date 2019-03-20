55dc6c5730b6fd1fdb6fb361f854a68bf8230181
github username <github email address>
2019-03-20 12:12:11 -0400

    render BoggleRoundBuilder in index.js instead of BoggleGame

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import BoggleGame from './containers/BoggleGame';
+import BoggleRoundBuilder from './containers/BoggleRoundBuilder';
 
 ReactDOM.render(
-  <BoggleGame />,
+  <BoggleRoundBuilder />,
   document.getElementById('root')
 );
