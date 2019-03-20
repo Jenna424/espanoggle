f28a4d83153373060e78272568e3603d1ab337bf
github username <github email address>
2019-03-19 20:39:37 -0400

    add ternary logic using props.viewable to trigger modal animation/change opacity

--- client/src/components/UI/Modal/Modal.js
+++ client/src/components/UI/Modal/Modal.js
@@ -2,8 +2,11 @@ import React from 'react';
 import styles from './Modal.module.css';
 
 const Modal = props => (
-  <div className={styles.modal}>
-    {props.children}
+  <div 
+    className={styles.modal} 
+    style={{transform: props.viewable ? 'translateY(0)' : 'translateY(-100vh)', 
+    opacity: props.viewable ? '1' : '0'}}>
+   {props.children}
   </div>
 )
 
@@ -13,4 +16,8 @@ export default Modal;
 // Modal functional component just receives props and returns JSX,
 // namely, a <div> to wrap around dynamic content, i.e., props.children
 // to provide styling
+
+// vh is a unit that refers to viewport height
+// consequently, the modal will slide off the screen
+// opacity '0' means it's not visible
 */}
