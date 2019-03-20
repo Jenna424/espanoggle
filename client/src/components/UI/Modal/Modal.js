d644871bc1508dc937d25c037f4d1a6ee6182b25
github username <github email address>
2019-03-20 13:19:26 -0400

    edit my Modal comments

--- client/src/components/UI/Modal/Modal.js
+++ client/src/components/UI/Modal/Modal.js
@@ -4,7 +4,7 @@ import Backdrop from '../Backdrop/Backdrop';
 
 const Modal = props => (
   <Fragment>
-    <Backdrop viewable={props.viewable} hide={props.closed}/> // If the Modal is displayed, the Backdrop is also displayed
+    <Backdrop viewable={props.viewable} hide={props.closed}/>
     <div
       className={styles.modal}
       style={{transform: props.viewable ? 'translateY(0)' : 'translateY(-100vh)', 
@@ -16,12 +16,11 @@ const Modal = props => (
 
 export default Modal;
 
-{/*
+
+// If the Modal is displayed, the Backdrop is also displayed
 // Modal functional component just receives props and returns JSX,
-// namely, a <div> to wrap around dynamic content, i.e., props.children
-// to provide styling
+// namely, a <div> that wraps around dynamic content, i.e., props.children, to provide styling
 
 // vh is a unit that refers to viewport height
 // consequently, the modal will slide off the screen
 // opacity '0' means it's not visible
-*/}
