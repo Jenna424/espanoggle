82297fc759e94adb299b97169a96087fe915a2e0
github username <github email address>
2019-03-20 13:20:00 -0400

    import & render BoggleGame component in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import BoggleRoundBuilder from './containers/BoggleRoundBuilder';
+import BoggleGame from './containers/BoggleGame';
 
 ReactDOM.render(
-  <BoggleRoundBuilder />,
+  <BoggleGame />,
   document.getElementById('root')
 );
