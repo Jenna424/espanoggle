aacedf45d2e96dc4a969d8b3d9fe6667b8b12934
github username <github email address>
2019-03-20 21:04:24 -0400

    change Presentada to Formada; change prop to handleButtonClick

--- client/src/components/PalabraFormada/PalabraFormada.js
+++ client/src/components/PalabraFormada/PalabraFormada.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Button from '../UI/Button/Button';
 
-const PalabraPresentada = ({ palabraCreada, enviarPalabra }) => {
+const PalabraFormada = ({ palabraCreada, enviarPalabra }) => {
   return (
   	<div style={{textAlign: 'center', paddingTop: '10px'}}>
   	  {palabraCreada.length > 0 && <p style={{color: 'navy'}}><strong>Palabra Creada</strong>:&nbsp;{palabraCreada}</p>}
-  	  {palabraCreada.length > 2 && <Button buttonClick={enviarPalabra} buttonType="enviar">ENVIAR</Button>}
+  	  {palabraCreada.length > 2 && <Button handleButtonClick={enviarPalabra} buttonType="enviar">ENVIAR</Button>}
   	</div>
   )
 }
 
-export default PalabraPresentada;
+export default PalabraFormada;
