2ff0cea27bfc8a5a971df06093bffcc8e40dd038
github username <github email address>
2019-03-20 21:18:28 -0400

    render PalabrasPresentadas in ScoreSummary; edit props

--- client/src/components/ScoreSummary/ScoreSummary.js
+++ client/src/components/ScoreSummary/ScoreSummary.js
@@ -1,32 +1,32 @@
 import React, { Fragment } from 'react';
 import styles from './ScoreSummary.module.css';
 import Button from '../../components/UI/Button/Button';
+import PalabrasPresentadas from '../../components/PalabrasPresentadas/PalabrasPresentadas';
 
 const ScoreSummary = ({ palabrasFormadas, onPlayAgain, onDeclinePlayAgain }) => {
-  const palabrasGanadas = Object.keys(palabrasFormadas).map(palabra =>
-    <li className={styles.word} key={palabra}><strong>{palabra}</strong>: {palabrasFormadas[palabra]}</li>
-  )
-
   return (
     <Fragment>
       <p style={{color: 'red'}}><strong>¡Se acabó el juego!</strong></p>
 
-      <h3>El Recuento de Puntos</h3>
+      <PalabrasPresentadas palabrasFormadas={palabrasFormadas} />
 
-      <p><em>A continuación, podés encontrar una lista de palabras y sus respectivos valores númericos:</em></p>
-
-      <ul className={styles.scrollable}>
-        {palabrasGanadas}
-      </ul>
+      <br />
 
       <strong>Puntuación Total</strong>:
 
-      <p>Vos has conseguido una suma de {Object.values(palabrasFormadas).reduce((acc, number) => acc + number, 0)} puntos.</p>
+      <p>
+        Vos has conseguido una suma de {Object.values(palabrasFormadas).map(stringNum => parseInt(stringNum)).reduce((acc, number) => acc + number, 0)} puntos.
+      </p>
+
+      {Object.keys(palabrasFormadas).length > 0 && 
+      <p>La mejor jugada: {Object.keys(palabrasFormadas).reduce((palabra1, palabra2) => 
+        palabrasFormadas[palabra1] > palabrasFormadas[palabra2] ? palabra1 : palabra2)}
+      </p>}
 
       <p>¿Querés jugar al Españoggle otra vez?</p>
 
-      <Button buttonClick={onPlayAgain} buttonType="aprobar">&nbsp;&nbsp;SÍ&nbsp;&nbsp;</Button>&nbsp;
-      <Button buttonClick={onDeclinePlayAgain} buttonType="rechazar">NO</Button>
+      <Button handleButtonClick={onPlayAgain} buttonType="aprobar">&nbsp;&nbsp;SÍ&nbsp;&nbsp;</Button>&nbsp;
+      <Button handleButtonClick={onDeclinePlayAgain} buttonType="rechazar">NO</Button>
     </Fragment>
   )
 }
