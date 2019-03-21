07cbf21a80b136071e3866c27c4a8c8bd4b363b0
github username <github email address>
2019-03-20 21:54:46 -0400

    conditionally display instructions in PalabrasPresentadas

--- client/src/components/PalabrasPresentadas/PalabrasPresentadas.js
+++ client/src/components/PalabrasPresentadas/PalabrasPresentadas.js
@@ -1,18 +1,28 @@
 import React, { Fragment } from 'react';
 import styles from './PalabrasPresentadas.module.css';
 
-const PalabrasPresentadas = ({ palabrasFormadas }) => {
+const PalabrasPresentadas = ({ palabrasFormadas, palabraCreada }) => {
   const puntosPorCadaPalabra = Object.keys(palabrasFormadas).map(palabra =>
   	<li className={styles.palabra} key={palabra}><strong>{palabra}</strong>: {palabrasFormadas[palabra]}</li>
   )
 
   return (
     <div>
-      <h3>El Recuento de Puntos</h3>
-      <p><em>A continuación, podés encontrar una lista de palabras y sus respectivos valores númericos:</em></p>
-      <ul className={styles.scrollable}>
-        {puntosPorCadaPalabra}
-      </ul>
+      {Object.keys(palabrasFormadas).length > 0 &&
+        <Fragment>
+          <h3>El Recuento de Puntos</h3>
+          <p><em>A continuación, podés encontrar una lista de palabras y sus respectivos valores númericos:</em></p>
+          <ul className={styles.scrollable}>
+            {puntosPorCadaPalabra}
+          </ul>
+        </Fragment>
+      }
+
+      <br />
+
+      {palabraCreada === '' && Object.keys(palabrasFormadas).length === 0 &&
+        <span><em>Haz clic en los cubos para deletrear una palabra válida en español.</em></span>
+      }
     </div>
   )
 }
