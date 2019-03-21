f8a19f53b1a5088de18e3d15c476bbbcfde89517
github username <github email address>
2019-03-20 20:48:04 -0400

    edit notes; change prop to handleButtonClick

--- client/src/components/UI/Button/Button.js
+++ client/src/components/UI/Button/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Button.module.css';
 
 const Button = props => (
-  <button onClick={props.buttonClick} className={[styles.boggleButton, styles[props.buttonType]].join(' ')}>
+  <button onClick={props.handleButtonClick} className={[styles.boggleButton, styles[props.buttonType]].join(' ')}>
     {props.children}
   </button>
 );
@@ -10,15 +10,15 @@ const Button = props => (
 export default Button;
 
 {/*
-// I created this separate Button functional component so that I can wrap multiple buttons
-// used throughout my application with some shared standard styling
+// Above, I created a separate Button functional component so that I can wrap multiple buttons
+// used throughout my application with some shared styling
 // This allows me to keep my code DRY
-// However, I do need SOME variation in styling for different types of buttons, 
-// e.g. start button, cancel button, play again button, etc.
+// However, I do need some variation in styling for different types of buttons, 
+// e.g., botón de inicio, botón para jugar de nuevo, etc.
 
-// Self-reminder: I must pass a string to the className property of the <button>
+// I must pass a string to the className property of the <button>
 // I added an array because I always want to assign the standard class of 'boggleButton',
-// but then I want to conditionally add specific classes, such as 'success', 'warning' or 'error'
-// I dynamically extract the class I want using the buttonType prop.
-// Then I convert the array of string elements into a single string by joining them with a space
+// but I also want to conditionally add specific classes, such as 'aprobar', 'rechazar', 'iniciar', 'enviar'
+// I dynamically extract the class I want by using the buttonType prop.
+// I then convert the array of string elements into a single string by joining them with a space
 */}
