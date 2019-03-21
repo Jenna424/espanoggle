69ead2fc19d27dddcb2ff7024413d7791ac8cb88
github username <github email address>
2019-03-20 21:02:04 -0400

    change PalabraPresentada files to PalabraFormada files

--- client/src/components/PalabraFormada/PalabraFormada.js
+++ client/src/components/PalabraFormada/PalabraFormada.js
@@ -5,7 +5,7 @@ const PalabraPresentada = ({ palabraCreada, enviarPalabra }) => {
   return (
   	<div style={{textAlign: 'center', paddingTop: '10px'}}>
   	  {palabraCreada.length > 0 && <p style={{color: 'navy'}}><strong>Palabra Creada</strong>:&nbsp;{palabraCreada}</p>}
-  	  {palabraCreada.length >= 3 && <Button onClick={enviarPalabra} buttonType="enviar">ENVIAR</Button>}
+  	  {palabraCreada.length > 2 && <Button buttonClick={enviarPalabra} buttonType="enviar">ENVIAR</Button>}
   	</div>
   )
 }
