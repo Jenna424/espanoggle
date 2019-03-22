09807cefdc8f2e9b9f41c12b6e70c17cae236df7
github username <github email address>
2019-03-22 08:47:17 -0400

    add Cube propTypes for cube, isClickable, handleCubeClicked; move export

--- client/src/components/Cube/Cube.js
+++ client/src/components/Cube/Cube.js
@@ -2,7 +2,7 @@ import React, { PureComponent } from 'react';
 import styles from './Cube.module.css';
 import PropTypes from 'prop-types';
 // Here, I'm extending PureComponent to avoid unnecessary re-renders of all cubes when a single cube is clicked. Only the cube that was clicked (and contiguous cubes) should be affected.
-class Cube extends PureComponent {
+export default class Cube extends PureComponent {
   handleOnClick = () => {
     if (this.props.isClickable(this.props.cube)) {
       this.props.handleCubeClicked(this.props.cube)
@@ -21,5 +21,8 @@ class Cube extends PureComponent {
   }
 }
 
-export default Cube;
-
+Cube.propTypes = {
+  cube: PropTypes.object.isRequired,
+  isClickable: PropTypes.func.isRequired,
+  handleCubeClicked: PropTypes.func.isRequired
+}
