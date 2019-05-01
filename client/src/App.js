09e253a3ce014e5b81356f9ee827df2f468029ab
github username <github email address>
2019-05-01 16:30:28 -0400

    change Juego to BoggleGame in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,13 @@ import React, { Fragment } from 'react';
 import { Route } from 'react-router-dom';
 import Navbar from './components/UI/Navbar/Navbar';
 import Reglas from './components/Reglas/Reglas';
-import Juego from './containers/Juego';
+import BoggleGame from './containers/BoggleGame';
 
 const App = () => {
   return (
     <Fragment>
       <Navbar />
-      <Route exact path="/" component={Juego} />
+      <Route exact path="/" component={BoggleGame} />
       <Route path="/reglas" component={Reglas} />
     </Fragment>
   )
@@ -17,7 +17,7 @@ const App = () => {
 export default App;
 
 {/*
-// For implementation without drag-to-select, add:
-// import BoggleGame from './containers/BoggleGame';
-// <Route exact path="/" component={BoggleGame} />
+// For the drag-to-select version, replace BoggleGame import statement and route with:
+// import Juego from './containers/Juego';
+// <Route exact path="/" component={Juego} />
 */}
