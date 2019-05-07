9ed3ade0457d7304518650590f3ed6e100310084
github username <github email address>
2019-05-07 18:02:04 -0400

    update App.js for drag-to-select

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import React, { Fragment } from 'react';
 import { Route } from 'react-router-dom';
 import Navbar from './components/UI/Navbar/Navbar';
 import Reglas from './components/Reglas/Reglas';
-import BoggleGame from './containers/BoggleGame';
+//import BoggleGame from './containers/BoggleGame';
+import Juego from './containers/Juego';
 
 const App = () => {
   return (
     <Fragment>
       <Navbar />
-      <Route exact path="/" component={BoggleGame} />
+      <Route exact path="/" component={Juego} />
       <Route path="/reglas" component={Reglas} />
     </Fragment>
   )
@@ -17,6 +18,10 @@ const App = () => {
 export default App;
 
 {/*
+// To revert back to click selection, replace Juego import statement and route with:
+// import BoggleGame from './containers/BoggleGame';
+// <Route exact path="/" component={BoggleGame} />
+
 // For the drag-to-select version, replace BoggleGame import statement and route with:
 // import Juego from './containers/Juego';
 // <Route exact path="/" component={Juego} />
